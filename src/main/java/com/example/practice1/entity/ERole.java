c34a193333d1d595c469f7ec926b37bfdf6f7b57
layanski <layanskiamalanathan@gmail.com>
2024-11-29 15:23:34 +0100

    roles and authorization

---
+++ src/main/java/com/example/practice1/entity/ERole.java
@@ -0,0 +1,7 @@
+package com.example.practice1.entity;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_MODERATOR
+}
---
+++ src/main/java/com/example/practice1/entity/Role.java
@@ -0,0 +1,40 @@
+package com.example.practice1.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, unique = true, nullable = false)
+    private ERole name;
+
+    // Constructors, Getters, and Setters
+    public Role() {
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
+}
